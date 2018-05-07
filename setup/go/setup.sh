2055e842f34736e2a5447c06e4c90e75bccc3479
Chris Rogus <rogusdev@gmail.com>
2018-05-06 22:13:34 -0400

    setup all langs script and fix go for PATH

--- setup/go/setup.sh
+++ setup/go/setup.sh
@@ -3,4 +3,5 @@ sudo rm -rf /usr/local/go
 wget -L https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz \
   && sudo tar -C /usr/local -xzf go*.tar.gz
 
-export PATH=/usr/local/go/bin:$PATH
+echo 'export PATH="/usr/local/go/bin:$PATH"' >> $HOME/.profile
+export PATH="/usr/local/go/bin:$PATH"
