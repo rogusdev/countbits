aa16a6a935741ba6c772ae77d4cf292696054f71
Jason Penny <jason.penny@braze.com>
2020-10-01 11:37:43 -0400

    Use a bufio.NewWriter for countbits in go

--- countbits/go/countbits.go
+++ countbits/go/countbits.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-//    "encoding/binary"
-    "encoding/json"
-    "fmt"
-    "os"
-    "strconv"
-    "time"
+	//    "encoding/binary"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 // must be less than 126 to be a positive int in a byte!
@@ -15,78 +16,86 @@ const MAXBITS = 31
 
 //const BOT = -2147483648;
 //const TOP = 2147483647;
-const BOT = -147483648;
-const TOP = 147483647;
-
+const BOT = -147483648
+const TOP = 147483647
 
 func elapsed_ms(start time.Time) int64 {
-    return int64(time.Since(start) / time.Millisecond)
+	return int64(time.Since(start) / time.Millisecond)
 }
-
 
 func main() {
-    // https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
-    // https://golang.org/pkg/time/#Now
-    start := time.Now()
+	// https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
+	// https://golang.org/pkg/time/#Now
+	start := time.Now()
 
-//    func elapsed_ms() int64 {
-//        return time.Since(start) / time.Millisecond
-//    }
+	//    func elapsed_ms() int64 {
+	//        return time.Since(start) / time.Millisecond
+	//    }
 
-    // https://gobyexample.com/maps
-    // https://tour.golang.org/basics/11
-    times := make(map[string]int64)
+	// https://gobyexample.com/maps
+	// https://tour.golang.org/basics/11
+	times := make(map[string]int64)
 
-    times["start"] = elapsed_ms(start)
+	times["start"] = elapsed_ms(start)
 
-    // https://gobyexample.com/arrays
-    // https://medium.com/learning-the-go-programming-language/bit-hacking-with-go-e0acee258827
-    //d2 := []byte{115, 111, 109, 101, 10}
-    var b [MAXBITS]int
-    for i := uint(0); i < MAXBITS; i++ { b[i] = 1 << i }
+	// https://gobyexample.com/arrays
+	// https://medium.com/learning-the-go-programming-language/bit-hacking-with-go-e0acee258827
+	//d2 := []byte{115, 111, 109, 101, 10}
+	var b [MAXBITS]int
+	for i := uint(0); i < MAXBITS; i++ {
+		b[i] = 1 << i
+	}
 
-    times["array"] = elapsed_ms(start)
+	times["array"] = elapsed_ms(start)
 
-    // https://gobyexample.com/writing-files
-    f, err := os.Create("counts.bin")
-    if err != nil { panic(err) }
-    defer f.Close()
+	// https://gobyexample.com/writing-files
+	f, err := os.Create("counts.bin")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    times["file"] = elapsed_ms(start)
+	w := bufio.NewWriter(f)
 
-    var c byte
-    for j := BOT; j < TOP; j++ {
-        if (j % 10000000) == 0 { times[strconv.Itoa(j)] = elapsed_ms(start) }
+	times["file"] = elapsed_ms(start)
 
-        if j < 0 {
-            c = 1
-        } else {
-            c = 0
-        }
+	var c byte
+	for j := BOT; j < TOP; j++ {
+		if (j % 10000000) == 0 {
+			times[strconv.Itoa(j)] = elapsed_ms(start)
+		}
 
-        for i := uint(0); i < MAXBITS; i++ {
-            c += byte((j & b[i]) >> i)
-        }
+		if j < 0 {
+			c = 1
+		} else {
+			c = 0
+		}
 
-        // https://stackoverflow.com/questions/16888357/convert-an-integer-to-a-byte-array
-        //bs := make([]byte, 4)
-        //binary.LittleEndian.PutUint32(bs, 31415926)
+		for i := uint(0); i < MAXBITS; i++ {
+			c += byte((j & b[i]) >> i)
+		}
 
-        _, err := f.Write([]byte{c})
-        if err != nil { panic(err) }
-    }
+		// https://stackoverflow.com/questions/16888357/convert-an-integer-to-a-byte-array
+		//bs := make([]byte, 4)
+		//binary.LittleEndian.PutUint32(bs, 31415926)
 
-    // last positive int is for sure MAXBITS on bits
-    //  we have to do this out of loop or else int rolls around and becomes infinite loop!
-    f.Write([]byte{byte(MAXBITS)})
+		_, err := w.Write([]byte{c})
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    times["end"] = elapsed_ms(start)
-    times["average"] = (times["end"] - times["file"]) / int64(len(times) - 4);
+	// last positive int is for sure MAXBITS on bits
+	//  we have to do this out of loop or else int rolls around and becomes infinite loop!
+	w.Write([]byte{byte(MAXBITS)})
+	w.Flush()
 
-    // https://stackoverflow.com/questions/24652775/convert-go-map-to-json
-    // https://blog.golang.org/json-and-go
-    // https://golang.org/pkg/encoding/json/
-    jsonBytes, _ := json.Marshal(times)
+	times["end"] = elapsed_ms(start)
+	times["average"] = (times["end"] - times["file"]) / int64(len(times)-4)
+
+	// https://stackoverflow.com/questions/24652775/convert-go-map-to-json
+	// https://blog.golang.org/json-and-go
+	// https://golang.org/pkg/encoding/json/
+	jsonBytes, _ := json.Marshal(times)
 	fmt.Println(string(jsonBytes))
 }
-
